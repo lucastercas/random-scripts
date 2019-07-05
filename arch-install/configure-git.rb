74da6653c6e68586823b720ba06982f647292c80
lucastercas <lucasmtercas@gmail.com>
2019-07-05 20:12:22 +0000

    feature(arch-install): Update configure-git, add execute permission

--- arch-install/configure-git.rb
+++ arch-install/configure-git.rb
@@ -1,9 +1,7 @@
-#!/usr/bin/env ruby
-
 # Usage: ./configure-git.rb <email> <username>
 # Ex: ./configure-git.rb lucasmtercas@gmail.com lucastercas
 
 email = ARGV[0]
 username = ARGV[1]
-`git config --global user.email ""`
-`git config --global user.name ""`
+`git config --global user.email #{email}`
+`git config --global user.name #{username}`
