2dcc9f2c6884647507d34e9b70da72b34fbcc0d9
Lucas Terças <lucasmtercas@gmail.com>
2019-08-03 00:52:58 -0300

    feat(fonts): Add hack font

--- arch-install/configure-packages.rb
+++ arch-install/configure-packages.rb
@@ -5,15 +5,30 @@
 # Disclaimer: This script is for personal use only, and there are steps that
 # are private sensitive that are not here, so do not blame me if you use it.
 #
-puts('Installing Arch Packages')
-default_packages_file = File.open("default-packages.txt")
-
-default_packages = []
-default_packages_file.each_line do |line|
-  if line[0] != '#' then
-    default_packages.append(line.gsub("\n", ''))
+def defaultPackages()
+  puts('Installing Arch Packages')
+  default_packages_file = File.open("default-packages.txt")
+  default_packages = []
+  default_packages_file.each_line do |line|
+    if line[0] != '#' then
+      default_packages.append(line.gsub("\n", ''))
+    end
   end
+  default_packages = default_packages.join(' ')
+  system("sudo pacman -S #{default_packages}")
 end
-default_packages = default_packages.join(' ')
 
-system("sudo pacman -S #{default_packages}")
+def aurPackages()
+  puts('Installing AUR Packages')
+  aur_packages_file = File.open('aur-packages.txt')
+  aur_packages = []
+  aur_packages_file.each_line do |line|
+    if line[0] != '#' then
+      aur_packages.append(line.gsub("\n", ''))
+    end
+  end
+  aur_packages = aur_packages.join(' ')
+  system("yay -S #{aur_packages}")
+end
+
+aurPackages()
